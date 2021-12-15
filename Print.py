342de27234de2009e7618965c6a0ab420e23d615
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2021-12-15 23:51:42 +0300

    Update Print.py

--- Print.py
+++ Print.py
@@ -1,1 +1,30 @@
+strvar="python"
+print(strvar)
+strvar=strvar+"\togren"
+print(strvar)
+print("naber")
+#yazdırmaları bu 3 şekilde yapabiliriz
+
+-------------------------------------------------------------------
 print("benim adım:{ad},yaşım:{yaş}".format(ad='ömer',yaş=32))
+-------------------------------------------------------------------
+print(5/4)
+-------------------------------------------------------------------
+strvar ="python"
+print(strvar)
+strvar=strvar+"ogren"
+print(strvar)
+len(strvar)
+------------------------------------------------------------------
+strvar[3]
+#0-1-2-3 şeklinde gidiyor bu yüzden 3. harf h oldu
+------------------------------------------------------------------
+print(strvar.upper())
+#tüm harfleri büyük yapar
+print(strvar.lower())
+#tüm harfleri küçük yapar
+print(strvar.split("o"))
+#o geçen yerleri atlar
+------------------------------------------------------------------
+len("naber")
+-----------------------------------------------------------------
